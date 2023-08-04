8993c507cce7f272e31f78e72312b2c81600b95c
João Grilo <joaogrilo2222@gmail.com>
2023-08-04 18:09:51 +0100

    Base code Finish

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Encrypted Chat Python
  Encrypted Chat made in Python
+
+
+## .env File
+1. Create .env file
+2. Put the following code:
+```python
+IPADRESS = "IP_ADRESS"
+PORT = "ANY_PORT"
+```
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import threading
 
 import rsa
 
+public_key,  private_key = rsa.newkeys(1024)
+public_partner = None
+
+
 load_dotenv('.env')
 ip = os.getenv('IPADRESS')
 port = int(os.getenv('PORT'))
@@ -17,11 +21,15 @@ if choise == '1':
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((ip, port))
     server.listen()
-
     client, _ = server.accept()
+    client.send(public_key.save_pkcs1("PEM"))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
 elif choise == '2':
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("192.168.1.78", 9999))
+    client.connect((ip, port))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+    client.send(public_key.save_pkcs1("PEM"))
+
 
 else:
     exit()
@@ -29,12 +37,12 @@ else:
 def sending_messages(c):
     while True:
         message = input("")
-        c.send(message.encode())
+        c.send(rsa.encrypt(message.encode(), public_partner))
         print ('You: ' + message)
 
 def receiving_messages(c):
     while True:
-        print ('Partner: ' + c.recv(1024).decode())
+        print ('Partner: ' + rsa.decrypt(c.recv(1024), private_key).decode())
 
 
 threading.Thread(target=sending_messages, args=(client, )).start()
